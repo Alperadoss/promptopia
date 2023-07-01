8d06662a8cffdd5ac97a1fb808bc1620c131ca77
Alper Yazagan <alperyazagan@gmail.com>
2023-07-01 23:43:32 +0300

    home page stuff

--- utils/database.js
+++ utils/database.js
@@ -4,4 +4,16 @@ let isConnected = false;
 
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
+
+  if (isConnected) {
+    console.log("mongoDB is already connected");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {}
 };
