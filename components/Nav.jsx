13fb693f3cdfa6c0e5f9105ab88b4e20570977da
Alper Yazagan <alperyazagan@gmail.com>
2023-07-01 16:46:29 +0300

    page structure

---
+++ components/Nav.jsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+
+const Nav = () => {
+  const isUserLoggedIn = true;
+
+  return (
+    <nav className="flex-between w-full pt-3 mb-16">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="promptopia logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo-text">Promptopia</p>
+      </Link>
+
+      {/*Mobile Navigation*/}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
+
+            <button type="button" onClick={signOut} className="outline_btn">
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                alt="profile pic"
+                className="rounded-full"
+              />
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
