1afd467291b5ab9dd3452377983b7f6e72242efc
Alper Yazagan <alperyazagan@gmail.com>
2023-07-10 16:44:59 +0300

    route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 import User from "@models/user";
-import { connectToDB } from "@utils/database";
+import { connectToDB } from "@utils/database"; 
 
 const handler = NextAuth({
   providers: [
@@ -11,10 +11,29 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+        email: session.user.email
+    })
+    session.user.id = sessionUser._id.toString(); 
+
+    return session;
+  },
   async signIn({ profile }) {
     try {
       await connectToDB();
+      //check if user exists
+      const userExists = await User.findOne({
+        email: profile.email,
+        //if not, create a new user
+        if( !userExists ) {
+            await User.create({
+                email: profile.email,
+                username: profile.name.replace("", "").toLowerCase(),
+                image: profile.picture,
+            })
+        }
+      });
     } catch (error) {}
   },
 });
